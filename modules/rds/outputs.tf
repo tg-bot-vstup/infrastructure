d62fc3e766672ea4780e5edcc18d10e8087d852e
bisspector <bisspector@gmail.com>
2022-02-03 22:31:31 +0200

    fix rds output

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,4 +1,4 @@
 output "db_url" {
-  value     = "postgresql://${aws_db_instance.default.username}:${aws_db_instance.default.password}@${aws_db_instance.default.endpoint}"
+  value     = "postgresql://${aws_db_instance.default.username}:${aws_db_instance.default.password}@${aws_db_instance.default.endpoint}/${aws_db_instance.default.name}"
   sensitive = true
 }
