f661127f4624188a59673f4debec913916217228
bisspector <bisspector@gmail.com>
2022-02-03 20:22:13 +0200

    fix rds iam in cluster

--- modules/cluster/iam.tf
+++ modules/cluster/iam.tf
@@ -38,3 +38,9 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
+
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
